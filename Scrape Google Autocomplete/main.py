b8e7a80837d359384e85f2cb7fdbd939a7da5967
Dimitry Zub <78694043+dimitryzub@users.noreply.github.com>
2021-04-23 10:31:11 +0300

    Create main.py

---
+++ Scrape Google Autocomplete/main.py
@@ -0,0 +1,11 @@
+import requests, json
+
+headers = {
+"User-Agent":
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+}
+
+response = requests.get('http://google.com/complete/search?client=firefox&q=minecraft is the best game', headers = headers)
+
+for result in json.loads(response.text)[1]:
+  print(result)
